6f96ddf4a7605aae40bdf1c7bce6be787553b517
Hughes <arwel.hughes@ndw2504mac.isis.cclrc.ac.uk>
2023-01-31 13:41:47 +0000

    Added check for folder permissions

--- Rascal_functions/rLoadProblem.m
+++ Rascal_functions/rLoadProblem.m
@@ -1,8 +1,15 @@
 function problem = rLoadProblem(path,name);
 
+% Check to see if the user has save permissions....
+[~,attr] = fileattrib(pwd);
+if ~attr.UserWrite
+    warning('no user save permissions on current directory');
+end
+
 cd(path);
 load(name);
-if strcmp(problem.path,path)~=1
+
+if (strcmp(problem.path,path)~=1) && attr.UserWrite
     problem.path = path;
     save(name,'problem');
 end
--- Rascal_functions/rascalMemoryCleanup.m
+++ Rascal_functions/rascalMemoryCleanup.m
@@ -4,9 +4,14 @@ function rascalMemoryCleanup()
 java.lang.System.gc();
 drawnow limitrate;
 
-evalin('base','save tempfile.mat');
-evalin('base','clear all');
-evalin('base','load tempfile.mat');
+[~,attr] = fileattrib(pwd);
+if ~attr.UserWrite
+    warning('no user save permissions on current directory');
+else
+    evalin('base','save tempfile.mat');
+    evalin('base','clear all');
+    evalin('base','load tempfile.mat');
+end
 
 
 
